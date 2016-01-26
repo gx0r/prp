a7e2d736f7e55fb98ed94e1523492ef2df0458a3
Grant Miner <xxgsoftware@gmail.com>
2016-01-25 20:41:24 -0600

    Use TLS

--- proxy.js
+++ proxy.js
@@ -1,14 +1,17 @@
+'use strict';
+const Promise = require('bluebird');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer();
-const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const net = require('net');
 const socket = new net.Server();
 const config = require('./config.json');
+const pem = Promise.promisifyAll(require('pem'));
 
 socket.listen(config.port);
 
-console.log('Listening on ' + config.port);
+console.log('https://localhost:' + config.port);
 
 try {
     process.setgid('nobody');
@@ -19,20 +22,33 @@ try {
     console.error(e);
 }
 
-var auth = 'Basic ' + new Buffer(config.username + ":" + config.password).toString('base64');
+Promise.coroutine(function* () {
+    const auth = 'Basic ' + new Buffer(config.username + ":" + config.password).toString('base64');
 
-function validAuth(req) {
-    return req.headers['authorization'] && req.headers['authorization'] === auth
-}
+    const keys = yield pem.createCertificateAsync({
+        days: 1,
+        selfSigned: true
+    }); // generate a cert/keypair on the fly
 
-http.createServer(function(req, res) {
-    if (!validAuth(req)) {
-        res.writeHead(401, {
-            'WWW-Authenticate': 'Basic realm="users"'
-        });
-        res.end();
-    } else
-        proxy.web(req, res, {
-            target: config.target
-        });
-}).listen(socket);
+    const options = {
+        key: keys.serviceKey,
+          cert: keys.certificate
+    };
+
+    function validAuth(req) {
+        return req.headers['authorization'] && req.headers['authorization'] === auth
+    }
+
+    https.createServer(options, function(req, res) {
+        if (!validAuth(req)) {
+            res.writeHead(401, {
+                'WWW-Authenticate': 'Basic realm="users"'
+            });
+            res.end();
+        } else
+            proxy.web(req, res, {
+                target: config.target
+            });
+    }).listen(socket);
+
+})();
